1a2c2afa6bcab1100d8454206399027964ccf729
Gaston <gaston.gachetegui@gmail.com>
2021-07-05 12:55:35 -0300

    Basic UI, TODO: styling

--- main.js
+++ main.js
@@ -7,6 +7,21 @@
     Papel>Roca
 -Decidir ganador
 */
+const buttons = document.querySelectorAll(`button`);
+const div = document.querySelector('.results');
+const scorediv = document.querySelector(`.score`);
+const computer= document.createElement('h3');
+const player= document.createElement('h3');
+scorediv.appendChild(computer);
+scorediv.appendChild(player);
+let playerscore = 0;
+let computerscore = 0;
+buttons.forEach(e => {
+    e.addEventListener(`click`, ()=>{
+        let result = playRound(e.className)
+        checkResultUpdateScore(result);
+    });
+});
 function computerPlay(){
     let rnumber=Math.floor(Math.random()*100)%3
     switch (rnumber) {
@@ -23,40 +38,69 @@ function computerPlay(){
             break;
     }
 }
-function oneRound(playerSelection, computerSelection=computerPlay()){
+function playRound(playerSelection, computerSelection=computerPlay()){
     playerselection = playerSelection.toLowerCase()
+    div.removeChild(div.childNodes[0]);
+    const result = document.createElement(`p`);
     if (playerSelection == computerSelection){
-        console.log(`Its a tie ${playerSelection} its the same as ${computerSelection}`)
+        result.textContent=`Its a tie ${playerSelection} its the same as ${computerSelection}`;
+        div.appendChild(result);
         return(`tie`)
     }else if (playerSelection == "rock" && computerSelection == "scissors"){
-        console.log(`You win ${playerSelection} beats ${computerSelection}`)
+        result.textContent=`You win ${playerSelection} beats ${computerSelection}`;
+        div.appendChild(result);
         return(`playerwon`)
     }else if (playerSelection == "scissors" && computerSelection=="paper"){
-        console.log(`You win ${playerSelection} beats ${computerSelection}`)
+        result.textContent=`You win ${playerSelection} beats ${computerSelection}`;
+        div.appendChild(result);
         return(`playerwon`)
     }else if(playerSelection=="paper"&&computerSelection=="rock"){
-        console.log(`You win ${playerSelection} beats ${computerSelection}`)
+        result.textContent=`You win ${playerSelection} beats ${computerSelection}`;
+        div.appendChild(result);
         return(`playerwon`)
     }else{
-        console.log(`You lose ${computerSelection} beats ${playerSelection}`)
+        result.textContent=`You lose ${computerSelection} beats ${playerSelection}`;
+        div.appendChild(result);
         return(`playerlost`)
     }
 }
-function game(){
-    let scorePlayer=0;
-    let scoreComputer=0;
-    for (let i = 0; i < 5; i++) {
-        let result=oneRound(prompt(`Choose your weapon`));
-        switch (result) {
-            case `playerwon`:
-                scorePlayer++
-                break;
-            case `playerlost`:
-                scoreComputer++;
-            default:
-                break;
+function checkWin(){
+    if (playerscore >= 5){
+        console.log(`Player won`);
+        if (confirm(`You won!!!!! Wanna play again?`)){
+            playerscore = 0;
+            computerscore = 0;
+            player.textContent = `Player won ${playerscore}`;
+            computer.textContent = `Computer won ${computerscore}`;
+        }else{
+            return 0;
+        }
+    }else if(computerscore >=5){
+        console.log(`Computer won`);
+        if (confirm(`The computer won. Wanna play again?`)){
+            playerscore = 0;
+            computerscore = 0;
+            player.textContent = `Player won ${playerscore}`;
+            computer.textContent = `Computer won ${computerscore}`;
+        }else{
+            return 0;
         }
     }
-    scorePlayer>scoreComputer?console.log(`Player won`):console.log(`Computer won`)
 }
-game()
+function checkResultUpdateScore(r){
+    switch (r) {
+        case 'playerwon':
+            playerscore++;
+            break;
+        case 'playerlost':
+            computerscore++;
+            break;
+        case 'tie':
+            break;
+        default:
+            break;
+    }
+    player.textContent = `Player won ${playerscore}`;
+    computer.textContent = `Computer won ${computerscore}`;
+    checkWin()
+}
